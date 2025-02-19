9031f44a52826f1ea14b2fd0fd4b5b93e6d2bc9b
Stigstille <stigstille@stigstille.uk>
2025-02-19 00:29:27 -0500

    Change soft-links to folders

---
+++ hypr/startHyprpaper.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+hyprpaper & 
+sleep 1
+
+WALLPAPER_DIR="$HOME/Pictures/Backgrounds/"
+CURRENT_WALL=$(hyprctl hyprpaper listloaded)
+
+# Get a random wallpaper that is not the current one
+WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
+
+# Apply the selected wallpaper
+hyprctl hyprpaper reload ,"$WALLPAPER"
