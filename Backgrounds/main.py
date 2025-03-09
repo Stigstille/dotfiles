b873ef9c2dd5ac344bd30f76a0022b2698eaa79f
Stigstille <stigstille@stigstille.uk>
2025-03-09 18:44:20 -0400

    Add logging, because stuff stops crashing once you start logging for some reason

--- Backgrounds/main.py
+++ Backgrounds/main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import logging
+import traceback
 from ytmusicapi import YTMusic
 import os, re, requests
 
@@ -7,35 +9,56 @@ ytmusic = YTMusic()
 
 previusSong = ""
 
-while True: 
-    song = os.popen('playerctl metadata --format "{{title}} - {{artist}}"').read()
-    if (previusSong != song) and (song != ""):
-        previusSong = song
-        try: 
-            search_results = ytmusic.search(song,"songs")
+log_file = os.path.expanduser('~/crash_log.txt')
+logging.basicConfig(
+    filename=log_file,
+    level=logging.INFO, 
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 
-            videoId = search_results[0]["videoId"]
-        except:
-            search_results = ytmusic.search(song)
-            videoId = search_results[0]["videoId"]
-        thumbnailInfo = search_results[0]["thumbnails"][0]
-        print(search_results[0]["title"])
-        thumbnail = thumbnailInfo["url"]
-        thumbnailWidth = thumbnailInfo["width"]
-        thumbnailHeight = thumbnailInfo["height"]
+def main():
+    previusSong = ""
+    while True: 
+        song = os.popen('playerctl metadata --format "{{title}} - {{artist}}"').read()
+        if (previusSong != song) and (song != ""):
+            previusSong = song
+            logging.info(f"Song changed to {song}")
+            try: 
+                search_results = ytmusic.search(song,"songs")
 
-        # please ignore my shitty regex
-        pattern = r"=w.{1,4}\-h.{1,4}-"
-        size = f"=w{thumbnailWidth*100}-h{thumbnailHeight*100}-"
+                videoId = search_results[0]["videoId"]
+            except:
+                search_results = ytmusic.search(song)
+                logging.warning(f"Song not found, searching for {search_results[0]['title']} instead")
+                videoId = search_results[0]["videoId"]
+            logging.debug(f"Video ID: {videoId}")
+            thumbnailInfo = search_results[0]["thumbnails"][0]
+            print(search_results[0]["title"])
+            thumbnail = thumbnailInfo["url"]
+            thumbnailWidth = thumbnailInfo["width"]
+            thumbnailHeight = thumbnailInfo["height"]
 
-        thumbnailResized = re.sub(pattern, size, thumbnail)
-        img_data = requests.get(thumbnailResized).content
-        with open('/tmp/album-art.jpg', 'wb') as handler:
-            handler.write(img_data)
+            # please ignore my shitty regex
+            pattern = r"=w.{1,4}\-h.{1,4}-"
+            size = f"=w{thumbnailWidth*100}-h{thumbnailHeight*100}-"
 
-        os.system(r'/usr/bin/sed -i --follow-symlinks "s/path = .*/path = \/tmp\/album-art.jpg/1" $HOME/.config/hypr/hyprlock.conf')
-        
-        os.system('swww img /tmp/album-art.jpg --transition-type random --transition-angle 25')
-    elif (previusSong != song) and (song == ""):
-        previusSong = song
-        os.system("~/.config/hypr/reloadHyprpaper.sh")
+            thumbnailResized = re.sub(pattern, size, thumbnail)
+            img_data = requests.get(thumbnailResized).content
+            with open('/tmp/album-art.jpg', 'wb') as handler:
+                handler.write(img_data)
+
+            os.system(r'/usr/bin/sed -i --follow-symlinks "s/path = .*/path = \/tmp\/album-art.jpg/1" $HOME/.config/hypr/hyprlock.conf')
+            
+            os.system('swww img /tmp/album-art.jpg --transition-type random --transition-angle 25')
+        elif (previusSong != song) and (song == ""):
+            previusSong = song
+            logging.info(f"No song playing")
+            os.system("~/.config/hypr/reloadHyprpaper.sh")
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        logging.error(traceback.format_exc())
+        logging.error(f'An error occurred: {str(e)}')
+        raise
